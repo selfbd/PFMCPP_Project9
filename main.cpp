339612ce88a456c7a2d384554b90e9ea19941d15
selfbd <bds_github@fastmail.fm>
2020-12-24 04:09:24 +0000

    Removed unnecessary code and comments after challenges

--- main.cpp
+++ main.cpp
@@ -49,19 +49,6 @@ private:
     float x{0}, y{0};
 };
 
-/*
-2) implement a RECURSIVE variadicHelper Function and a Wrapper class.
-
-3) your variadicHelper function needs to be a Recursive Variadic Template Function.
-    The recursion works by removing the first element from the Variadic Template Parameter Pack, doing something with it, and calling VariadicHelper with the rest of the Pack.
-
-6) make the body of both variadicHelper functions instantiate a Wrapper instance, which wraps the first function argument, and call the 'print()' member function of the Wrapper class.
-
-7) Then, recursively call VariadicHelper.
-
-9) be sure to forward your variadicHelper function arguments correctly and use Forwarding/R-Value references, as opposed to passing by const-ref or by copy.
-*/
-
 template<typename Type>
 struct Wrapper
 {
@@ -69,10 +56,6 @@ struct Wrapper
     { 
         std::cout << "Wrapper(" << typeid(val).name() << ")" << std::endl; 
     }
-
- /*
- 5) add a print() function to the Wrapper stub.
- */
 
     void print()
     {
@@ -82,15 +65,13 @@ private:
     Type val;
 };
 
-/*
-8) You will need to specialize the Wrapper class template to work with the Point class.
-*/
-
 template<>
 void Wrapper<Point>::print()
 {
     std::cout << "Wrapper::print(" << val.toString() << ")" << std::endl;
 }
+
+void variadicHelper(){}
 
 template<typename T, typename ...Args>
 void variadicHelper(T&& first, Args&& ... everythingElse)
@@ -99,18 +80,6 @@ void variadicHelper(T&& first, Args&& ... everythingElse)
     wrapper.print();
     variadicHelper( std::forward<Args>(everythingElse)... );
 }
-
-/*
-4) You'll need to write this single-template-parameter version of variadicHelper along with the variadic-template version of VariadicHelper.
-*/
-
-template<typename T>
-void variadicHelper(T&& first)
-{
-    Wrapper wrapper( std::forward<T>(first) );
-    wrapper.print();    
-}
-void variadicHelper(){}
 
 void extraTests()
 {
